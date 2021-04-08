6814373975c4e69500b1bb1e090b4f3e82ccd977
Avraz  Zebary <avrazebary@gmail.com>
2021-04-08 13:58:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ function moveDodgerRight() {
 }
 
 document.addEventListener("keydown", function(e) {
-  if (e.key === "ArrowLeft") {
-    moveDodgerLeft();
+  if (e.key === "ArrowRight") {
+    moveDodgerRight();
   }
 });
