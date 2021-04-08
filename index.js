9421d67519e3932e2f873fbad7fd37e8445e7c37
Avraz  Zebary <avrazebary@gmail.com>
2021-04-08 13:54:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 // Your code here
 
 let dodger = document.getElementById("dodger");
-dodger.style.backgroundColor = "#000000";
+dodger.style.backgroundColor = "#FF69B4";
+dodger.style.left = "180px";
+dodger.style.bottom = "0px";
